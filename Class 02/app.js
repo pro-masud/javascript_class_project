9f07a17a555c39a979fc2ca48fe73519bbacf469
pro-mannan <promasudbd@gmail.com>
2023-04-27 23:17:41 +0600

    conditional statment for project

---
+++ Class 02/app.js
@@ -0,0 +1,12 @@
+
+
+// user name checked now
+
+let userFirstName = prompt("First Name");
+let userLastName = prompt("Last Name");
+
+if( userLastName == "rana"){
+    console.log(`My name is ${ userFirstName } ${ userLastName }`);
+}else{
+    console.log(`My name is ${ userFirstName } ${ userLastName }`);
+}
