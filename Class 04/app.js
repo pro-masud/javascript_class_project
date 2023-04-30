04928a99fee0f3c16a029b38c755b923dba9b9e5
pro-mannan <promasudbd@gmail.com>
2023-05-01 02:02:04 +0600

    user defiend funtion complaite now

---
+++ Class 04/app.js
@@ -0,0 +1,13 @@
+// user defiend function
+
+function userDev(name, job){
+    console.log(`
+        
+        my name is ${ name } and I'm ${ job }
+
+    `); 
+}
+
+userDev('masud rana','PHP Developer');
+
+userDev('Mursalim Islam','Java Developer');
