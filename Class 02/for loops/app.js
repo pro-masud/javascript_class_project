974e3064f4f2eb79aa75a8a8ccdff9c733d035d0
pro-mannan <promasudbd@gmail.com>
2023-04-30 08:41:37 +0600

    learn loop for javascript

---
+++ Class 02/for loops/app.js
@@ -0,0 +1,13 @@
+
+    
+
+
+    for( let i = 100; i >= 1; i--){
+        if( i >= 70 ){
+            for(let a = 70; a <= 100; a++){
+                if( a % 2 !== 0){
+                    console.log(a);
+                }
+            }
+        }
+    }
